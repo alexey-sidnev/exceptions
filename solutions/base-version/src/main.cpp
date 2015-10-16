9d65632c2d9a16f3ef8e4b3e068aed2e49f2bb6e
Alexey A. Sidnev <alexey.sidnev@gmail.com>
2015-10-16 12:44:57 +0300

    Добавлена обработка исключений по умолчанию

--- solutions/base-version/src/main.cpp
+++ solutions/base-version/src/main.cpp
@@ -7,47 +7,52 @@
 #define TESTS_COUNT 10
 
 int main() {
-  A a;
-  B b;
-  srand(666);
-
-  for (int i = 0; i < TESTS_COUNT; i++) {
-    int size = 0;
-    unsigned char *size_as_uchar_array = (unsigned char *)&size;
-    for (int j = 0; j < sizeof(int); j++)
-      size_as_uchar_array[j] = rand() % 256;
-    while ( size > (1 << 19) )
-      size /= 2;
+  printf("Exceptions (start).\n");
+  try {
+    A a;
+    B b;
+    srand(666);
+    
+    for (int i = 0; i < TESTS_COUNT; i++) {
+      int size = 0;
+      unsigned char *size_as_uchar_array = (unsigned char *)&size;
+      for (int j = 0; j < sizeof(int); j++)
+        size_as_uchar_array[j] = rand() % 256;
+      while ( size > (1 << 19) )
+        size /= 2;
+      try {
+        Test1(size);
+      }
+      catch(MyException &e) {
+        e.WriteLog();
+      }
+    }
+    
+    for (int i = 0; i < TESTS_COUNT; i++) {
+      try {
+        Test2();
+      }
+      catch(MyException &e) {
+        e.WriteLog();
+      }
+    }
+    
     try {
-      Test1(size);
+      Test3(&b);
     }
     catch(MyException &e) {
       e.WriteLog();
     }
-  }
-
-  for (int i = 0; i < TESTS_COUNT; i++) {
+    
     try {
-      Test2();
+      Test3(&a);
     }
     catch(MyException &e) {
       e.WriteLog();
     }
-  }
-
-  try {
-    Test3(&b);
-  }
-  catch(MyException &e) {
-    e.WriteLog();
-  }
-
-  try {
-    Test3(&a);
-  }
-  catch(MyException &e) {
-    e.WriteLog();
-  }
+  } catch(...) { }
+  
+  printf("Exceptions (finish).\n");
 
   return 0;
 }
