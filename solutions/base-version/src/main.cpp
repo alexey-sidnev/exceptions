efec0295126b920ce0ca90611d305a508129b527
Alexey A. Sidnev <alexey.sidnev@gmail.com>
2015-10-26 21:11:05 +0300

    Delete some style checks

--- solutions/base-version/src/main.cpp
+++ solutions/base-version/src/main.cpp
@@ -12,7 +12,7 @@ int main() {
     A a;
     B b;
     srand(666);
-    
+
     for (int i = 0; i < TESTS_COUNT; i++) {
       int size = 0;
       unsigned char *size_as_uchar_array = (unsigned char *)&size;
@@ -27,7 +27,7 @@ int main() {
         e.WriteLog();
       }
     }
-    
+
     for (int i = 0; i < TESTS_COUNT; i++) {
       try {
         Test2();
@@ -36,14 +36,14 @@ int main() {
         e.WriteLog();
       }
     }
-    
+
     try {
       Test3(&b);
     }
     catch(MyException &e) {
       e.WriteLog();
     }
-    
+
     try {
       Test3(&a);
     }
@@ -53,7 +53,7 @@ int main() {
 
     try {
       double sum = 0.;
-      
+
       sum = Test4(10.1);
       printf("sum(10.1) =  %lf\n", sum);
 
@@ -64,7 +64,7 @@ int main() {
       e.WriteLog();
     }
   } catch(...) { }
-  
+
   printf("Exceptions (finish).\n");
 
   return 0;
