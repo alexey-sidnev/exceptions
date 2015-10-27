5aed6a8540f0ff4e092aea722fee76e976d0edc4
ksenya-sizova <sizova.ksuniy@mail.ru>
2015-10-27 22:36:08 +0300

    Гора бесполезных коммитов на низком старте

--- solutions/SizovaKsenya/src/MyException.cpp
+++ solutions/SizovaKsenya/src/MyException.cpp
@@ -1,30 +1,33 @@
 #include "MyException.h"
 
 void MyException::WriteLog() {
-	if (e != 0)
-		e -> WriteLog();
-	cout << str;
-	cout << endl;
+	
+  if (e != 0)
+    (*e).WriteLog();
+  cout << str;
+  cout << endl;
 }
 
 MyException::MyException(char *str0, MyException *e0) {
-	str = str0;
-	e = e0;
+
+  str = str0;
+  e = e0;
 }
 
 MyException::MyException(const MyException &E) {
-	if (E.str != 0) {
-		str = new char[strlen(E.str)];
-		strcpy_s(str, 300, E.str);
-	}
-	if (E.e != 0)
-		e = new MyException(*(E.e));
-	else
-		e = 0;
+
+  if (E.str != 0) {
+    str = new char[strlen(E.str)];
+	strcpy_s(str, 300, E.str);
+  }
+  if (E.e != 0)
+	e = new MyException(*(E.e));
+  else e = 0;
 }
 
 MyException::~MyException() {  
-	if (e != 0)  
-		delete e;  
-	delete[] str;  
+
+  if (e != 0)
+    delete e;
+  delete[] str;
 }  
