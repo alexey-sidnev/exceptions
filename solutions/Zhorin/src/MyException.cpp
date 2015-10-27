ec8fbe4b6f3b98150ba5704ce104575c2a790f53
Zhorin Sergey <ser4038772@mail.ru>
2015-10-27 22:25:02 +0300

    Почему ты тестируешь проект Ильи, а?

--- solutions/Zhorin/src/MyException.cpp
+++ solutions/Zhorin/src/MyException.cpp
@@ -1,36 +1,31 @@
 #include "MyException.h"
 
 void MyException::WriteLog() {
-
-	if (prev != 0)
-		(*prev).WriteLog();
-	cout << Log
-		<< endl;
+  if (prev != 0)
+    (*prev).WriteLog();
+  cout << Log << endl;
 }
-MyException::MyException(char*Log_1, MyException *prev_1){
-	Log = Log_1;
-	prev = prev_1;
+
+MyException::MyException(char*Log_1, MyException *prev_1) {
+  Log = Log_1;
+  prev = prev_1;
 
 }
+
 MyException::MyException(const MyException&ex){
-	if (ex.Log != 0)
-	{
-		Log = new char[strlen(ex.Log)];
-		strcpy_s(Log, 300, ex.Log);
-	}
-
-	if (ex.prev != 0)
-		prev = new MyException(*(ex.prev));
-	else
-		prev = 0;
-
+  if (ex.Log != 0) {
+    Log = new char[strlen(ex.Log)];
+    strcpy_s(Log, 300, ex.Log);
+  }
+  if (ex.prev != 0)
+    prev = new MyException(*(ex.prev));
+  else
+    prev = 0;
 }
+
 MyException::~MyException(){
-	{
-		if (prev != 0) delete prev;
-		delete[] Log;
-	}
-
+  {
+    if (prev != 0) delete prev;
+      delete[] Log;
+  }
 }
-
-
