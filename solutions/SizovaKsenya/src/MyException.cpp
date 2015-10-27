d7894dd8372f5ce546aafb08e7d2f47b90abc397
ksenya-sizova <sizova.ksuniy@mail.ru>
2015-10-27 22:46:47 +0300

    Помолимся за MyException.cpp

--- solutions/SizovaKsenya/src/MyException.cpp
+++ solutions/SizovaKsenya/src/MyException.cpp
@@ -1,7 +1,6 @@
 #include "MyException.h"
 
-void MyException::WriteLog() {
-	
+void MyException::WriteLog() { 
   if (e != 0)
     (*e).WriteLog();
   cout << str;
@@ -9,25 +8,23 @@ void MyException::WriteLog() {
 }
 
 MyException::MyException(char *str0, MyException *e0) {
-
   str = str0;
   e = e0;
 }
 
 MyException::MyException(const MyException &E) {
-
   if (E.str != 0) {
     str = new char[strlen(E.str)];
 	strcpy_s(str, 300, E.str);
   }
   if (E.e != 0)
 	e = new MyException(*(E.e));
-  else e = 0;
+  else 
+	  e = 0;
 }
 
-MyException::~MyException() {  
-
+MyException::~MyException() {
   if (e != 0)
     delete e;
   delete[] str;
-}  
+} 
