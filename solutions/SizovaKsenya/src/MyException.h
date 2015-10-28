688f86edfda95d7d87fa66cb790f85f9f0a1d7a4
ksenya-sizova <sizova.ksuniy@mail.ru>
2015-10-28 15:22:52 +0300

    Погнали

--- solutions/SizovaKsenya/src/MyException.h
+++ solutions/SizovaKsenya/src/MyException.h
@@ -9,44 +9,45 @@ class MyException {
  protected:
   MyException *e;
   char *str;
- public: 
+ public:
   void WriteLog();
   virtual ~MyException();
-  MyException(){}
+  MyException() {}
   MyException(char *str0, MyException *e0);
   MyException(const MyException&E);
 };
 
 class Exception1 : public MyException {
- public: 
+ public:
   Exception1(char *str1, MyException *e1) : MyException(str1, e1)
   {}
-  Exception1(const MyException &E) :MyException(E) 
+  Exception1(const MyException &E) :MyException( E)
   {}
 };
 
 class Exception2 : public MyException {
- public: 
-  Exception2(char *str1, MyException *e1) : MyException(str1, e1) 
+ public:
+  Exception2(char *str1, MyException *e1) : MyException(str1, e1)
   {}
-  Exception2(const MyException &E) :MyException(E) 
+  Exception2(const MyException &E) :MyException( E)
   {}
 };
 
 class Exception3 : public MyException {
- public: 
-  Exception3(char *str1, MyException *e1) : MyException(str1, e1) 
+ public:
+  Exception3(char *str1, MyException *e1) : MyException(str1, e1)
   {}
-  Exception3(const MyException &E) :MyException(E) 
+  Exception3(const MyException &E) :MyException( E)
   {}
 };
 
 class Exception4 : public MyException {
  public:
-  Exception4(char *str1, MyException *e1) : MyException(str1, e1) 
+  Exception4(char *str1, MyException *e1) : MyException(str1, e1)
   {}
-  Exception4(const MyException &E) :MyException(E) 
+  Exception4(const MyException &E) :MyException( E)
   {}
 };
 
 #endif  // SOLUTIONS_SIZOVAKSENYA_SRC_MYEXCEPTION_H_
+
