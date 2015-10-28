4131b3d5e4042da2751cc02251dc2776a7a13727
mariaPetrova18912 <maria18912@yandex.ru>
2015-10-28 21:54:41 +0300

    c

--- solutions/Petrova/src/MyException.cpp
+++ solutions/Petrova/src/MyException.cpp
@@ -1,33 +1,29 @@
 #include "MyException.h"
 
-MyException :: MyException(char *str1, MyException *ex1){
-	str = str1;
-	ex = ex1;
+MyException :: MyException(char *str1, MyException *ex1) {
+    str = str1;
+    ex = ex1;
 }
 
-MyException :: MyException(const MyException &EX){
-	if (EX.str != 0)
-	{
-		str = new char[strlen(EX.str)];
-		strcpy(str, EX.str);
-	}
-
-	if (EX.ex != 0)
-		ex = new MyException(*(EX.ex));
-	else
-		ex = 0;
-
+MyException :: MyException(const MyException &EX) {
+    if (EX.str != 0) {
+        str = new char[strlen(EX.str)];
+        strcpy(str, EX.str);
+    }
+    if (EX.ex != 0)
+        ex = new MyException(*(EX.ex));
+    else
+        ex = 0;
 }
 
-void MyException::WriteLog(){
-	if (ex != 0)
-		ex->WriteLog();
-
-	cout << str <<endl;
+void MyException::WriteLog() {
+    if (ex != 0)
+        ex->WriteLog();
+    cout << str <<endl;
 }
 
-MyException :: ~MyException(){
-	if (ex != 0)
-		delete ex;
-	delete []str;
+MyException :: ~MyException() {
+    if (ex != 0)
+        delete ex;
+    delete []str;
 }
